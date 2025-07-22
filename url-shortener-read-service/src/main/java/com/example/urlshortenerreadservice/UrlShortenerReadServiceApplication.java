4555ce2300aa358f4da99202542707ed8a4ead8a
Eugen Vashkevich <eugenvashkevich2@gmail.com>
2025-07-23 01:00:41 +0300

    Adds empty project for read service

---
+++ url-shortener-read-service/src/main/java/com/example/urlshortenerreadservice/UrlShortenerReadServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.urlshortenerreadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerReadServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UrlShortenerReadServiceApplication.class, args);
+  }
+}
---
+++ url-shortener-read-service/src/test/java/com/example/urlshortenerreadservice/UrlShortenerReadServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.urlshortenerreadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerReadServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
